115b32582669cb52f3dba8d1df193da6479c3380
shachar har zvi <shachar-har-zvi@hotmail.com>
2017-01-06 13:03:23 +0200

    fsa

---
+++ Pizza problem/Pizza problem/Parser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_problem
+{
+    public enum Engridient
+    {
+        Tomato,
+        Mushroom
+    }
+
+    class Parser
+    {
+        public void Parse()
+        {
+            using (StreamReader reader = new StreamReader("small.in"))
+            {
+                string first = reader.ReadLine();
+
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    char[] lineAsChar = line.ToCharArray();
+                    Engridient[] eng = new Engridient[line.Length];
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (lineAsChar[i] == 'M')
+                        {
+                            eng[i] = Engridient.Mushroom;
+                        }
+                        else
+                        {
+                            eng[i] = Engridient.Tomato;
+                        }
+                    }
+                }
+            }
+        }
+
+        class PizzaParams
+        {
+            public int MaxSliceSize { get; }
+            public int MinIngredientNum { get; }
+        }
+    }
+}
