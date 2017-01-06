4e5e5ade1ef62e88b6003268d0f77a18b605ad91
Gilthans <gilthans@gmail.com>
2017-01-06 13:18:27 +0200

    ..

--- Pizza problem/Pizza problem/PizzaSolverBase.cs
+++ Pizza problem/Pizza problem/PizzaSolverBase.cs
@@ -2,16 +2,28 @@
 {
 	public class PizzaSolverBase
 	{
-		protected readonly PizzaParams _pizza;
+		protected readonly PizzaParams Pizza;
 
 		public PizzaSolverBase(PizzaParams pizza)
 		{
-			_pizza = pizza;
+			Pizza = pizza;
 		}
 
 		public bool IsSliceValid(PizzaSlice slice)
 		{
-			
+			if (slice.Size > Pizza.MaxSliceSize)
+				return false;
+
+			int tomatoCount = 0;
+			int mushroomCount = 0;
+			for (int x = slice.TopLeft.X; x < slice.BottomRight.X; x++)
+			{
+				for (int y = slice.TopLeft.Y; y < slice.BottomRight.Y; y++)
+				{
+				}
+			}
+
+			return false;
 		}
 	}
 }
