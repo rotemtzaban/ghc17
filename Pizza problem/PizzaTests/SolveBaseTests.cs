295abbff585fe1f587eb78098f836fff8bde6072
Gilthans <gilthans@gmail.com>
2017-01-06 15:11:51 +0200

    Moved to sum area table

---
+++ Pizza problem/PizzaTests/SolveBaseTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaTests.Properties;
+using Pizza_problem;
+
+namespace PizzaTests
+{
+	[TestClass]
+	public class SolveBaseTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var parser = new Parser();
+			var pizza = parser.ParseData(Resources.example);
+
+			var solver = new PizzaSolverBase(pizza);
+
+			Assert.AreEqual(0, solver.GetMushroomsInSlice(new PizzaSlice(0, 0, 0, 0)));
+			Assert.AreEqual(1, solver.GetTomatoInSlice(new PizzaSlice(0, 0, 0, 0)));
+		}
+	}
+}
