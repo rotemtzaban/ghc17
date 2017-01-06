defe9e8e1691adf85ee8b51e1a34f10d67b48307
Gilthans <gilthans@gmail.com>
2017-01-06 13:59:47 +0200

    first

--- Pizza problem/Pizza problem/PizzaSolverBasic.cs
+++ Pizza problem/Pizza problem/PizzaSolverBasic.cs
@@ -14,12 +14,12 @@ namespace Pizza_problem
 
 		public IEnumerable<PizzaSlice> Solve()
 		{
-			return Solve(0, 0);
+			return Solve(0, 0, Pizza.XLength - 1, Pizza.YLength - 1);
 		}
 
-		public IEnumerable<PizzaSlice> Solve(int xStart, int yStart)
+		public IEnumerable<PizzaSlice> Solve(int xStart, int yStart, int maxX, int maxY)
 		{
-			if (xStart > Pizza.XLength || yStart > Pizza.YLength)
+			if (xStart > maxX || yStart > maxY)
 				return new PizzaSlice[] {};
 
 			var results = new List<PizzaSlice>();
@@ -30,26 +30,25 @@ namespace Pizza_problem
 				var currentSlice = new PizzaSlice(xStart, yStart, xEnd, yEnd);
 				if (IsSliceTooLarge(currentSlice))
 				{
-					// TODO: Infinite loop if last column is invalid :(
-					xStart = xEnd;
-					yEnd = yStart;
-					continue;
+					results.AddRange(Solve(xEnd + 1, yStart, maxX, yEnd));
+					results.AddRange(Solve(xStart, yEnd + 1, maxX, maxY));
+					return results;
 				}
 
 				if (IsEnoughIngredients(currentSlice))
 				{
 					results.Add(currentSlice);
-					results.AddRange(Solve(xEnd + 1, yStart));
-					results.AddRange(Solve(xStart, yEnd + 1));
+					results.AddRange(Solve(xEnd + 1, yStart, maxX, yEnd));
+					results.AddRange(Solve(xStart, yEnd + 1, maxX, maxY));
 					return results;
 				}
 
-				if (yEnd == Pizza.YLength - 1 && xEnd == Pizza.XLength - 1)
+				if (yEnd == maxY && xEnd == maxX)
 					return new PizzaSlice[] {};
 
-				if (yEnd == Pizza.YLength - 1)
+				if (yEnd == maxY)
 					xEnd++;
-				else if (xEnd == Pizza.XLength - 1)
+				else if (xEnd == maxX)
 					yEnd++;
 				else if (currentSlice.Height >= currentSlice.Width)
 					xEnd++;
