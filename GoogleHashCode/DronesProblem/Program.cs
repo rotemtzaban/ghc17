3e3fce8a96931289179769e7607d80f4e6010e0e
shachar har zvi <shachar-har-zvi@hotmail.com>
2017-01-20 15:35:59 +0200

    fdsf

--- GoogleHashCode/DronesProblem/Program.cs
+++ GoogleHashCode/DronesProblem/Program.cs
@@ -17,7 +17,10 @@ namespace DronesProblem
 		{
 			Runner<DronesInput, DronesOutput> runner = new Runner<DronesInput, DronesOutput>(new DronesParser(), new DronesSolver(), new DronesPrinter(), new DronesScoreCalculator());
 
-			runner.Run(DronesProblem.Properties.Resources.Example, "Example");
+			runner.Run(Properties.Resources.Example, "Example");
+			// runner.Run(Properties.Resources.busy_day, "busy_day");
+			// runner.Run(Properties.Resources.mother_of_all_warehouses, "mother_of_all_warehouses");
+			// runner.Run(Properties.Resources.redundancy, "redundancy");
 
 			runner.CreateCodeZip();
 		}
