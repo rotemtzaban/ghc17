7a07bd8c21e1970f78572dcf99251b26e8c0cef4
shachar har zvi <shachar-har-zvi@hotmail.com>
2017-01-20 14:20:34 +0200

    dsada

--- GoogleHashCode/DronesProblem/Program.cs
+++ GoogleHashCode/DronesProblem/Program.cs
@@ -18,36 +18,8 @@ namespace DronesProblem
             Runner<DronesInput, DronesOutput> runner = new Runner<DronesInput, DronesOutput>(new DronesParser(), new DronesSolver(), new DronesPrinter(), new DronesScoreCalculator());
 
             runner.Run(DronesProblem.Properties.Resources.Example, "Example");
+
+            runner.CreateCodeZip();
         }
-
-		private static void CreateCodeZip()
-		{
-			var tmpDirectoryName = "tmp";
-
-			var sourceDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
-			var tmpFolder = Path.Combine(sourceDir, tmpDirectoryName);
-			if (Directory.Exists(tmpFolder))
-				Directory.Delete(tmpFolder, true);
-			Directory.CreateDirectory(tmpFolder);
-			foreach (var codeFile in Directory.EnumerateFiles(sourceDir, "*.cs", SearchOption.AllDirectories))
-			{
-				var relative = codeFile.Substring(sourceDir.Length + 1);
-				if (relative.StartsWith("obj") || relative.StartsWith(tmpDirectoryName))
-					continue;
-				var target = Path.Combine(tmpFolder, relative);
-				var dir = Path.GetDirectoryName(target);
-				if (!Directory.Exists(dir))
-					Directory.CreateDirectory(dir);
-				File.Copy(codeFile, target);
-			}
-
-			var targetZip = Path.Combine(sourceDir, "out", "Code.zip");
-
-			if (File.Exists(targetZip))
-				File.Delete(targetZip);
-			ZipFile.CreateFromDirectory(tmpFolder, targetZip);
-
-			Directory.Delete(tmpFolder, true);
-		}
     }
 }
