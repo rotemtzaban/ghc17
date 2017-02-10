55341b4e658a5937f2c484a6f5f1d6738762329f
shachar har zvi <shachar-har-zvi@hotmail.com>
2017-02-10 12:45:57 +0200

    fsdf

--- GoogleHashCode/2015_Qualification_Test/UnitTest1.cs
+++ GoogleHashCode/2015_Qualification_Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2015_Qualification;
+using HashCodeCommon;
 
 namespace _2015_Qualification_Test
 {
@@ -9,6 +11,16 @@ namespace _2015_Qualification_Test
         [TestMethod]
         public void TestMethod1()
         {
+            Parser parser = new Parser();
+            ProblemInput input = parser.ParseFromData(Properties.Resources.TestInput);
+
+            Assert.AreEqual(2, input.Rows);
+            Assert.AreEqual(5, input.Columns);
+            Assert.AreEqual(2, input.Pools.Count);
+            Assert.AreEqual(1, input.UnavilableSlots.Count);
+            Assert.AreEqual(5, input.Servers.Count);
+
+            Assert.AreEqual(new Coordinate(0,0), input.UnavilableSlots[0]);
         }
     }
 }
