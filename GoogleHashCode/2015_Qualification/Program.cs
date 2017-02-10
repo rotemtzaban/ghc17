82d66252c6088ac613554d23bf41156beb97ae90
shachar har zvi <shachar-har-zvi@hotmail.com>
2017-02-10 11:44:04 +0200

    fsda

---
+++ GoogleHashCode/2015_Qualification/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015_Qualification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Runner<DronesInput, DronesOutput> runner = new Runner<DronesInput, DronesOutput>(new DronesParser(), new DronesSolver(), new DronesPrinter(), new DronesScoreCalculator());
+
+            //runner.Run(Properties.Resources.Example, "Example", 1, false);
+            //runner.Run(Properties.Resources.busy_day, "busy_day", 1, false);
+            //runner.Run(Properties.Resources.mother_of_all_warehouses, "mother_of_all_warehouses", 1, false);
+            //runner.Run(Properties.Resources.redundancy, "redundancy", 1, false);
+
+            //runner.CreateCodeZip();
+            Console.Read();
+        }
+    }
+}
---
+++ GoogleHashCode/DronesProblem/tmp/Warehouse.cs
@@ -0,0 +1,35 @@
+﻿using HashCodeCommon;
+using HashCodeCommon.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DronesProblem
+{
+    public class Warehouse : IndexedObject
+    {
+        public Coordinate Location { get; set; }
+
+        public Dictionary<Product, int> Products { get; set; }
+
+        public Warehouse(int index)
+            :base (index)
+        {
+        }
+
+        public Warehouse (Warehouse other)
+            :this (other.Index)
+        {
+            this.Location = other.Location;
+            this.Products = new Dictionary<DronesProblem.Product, int>();
+
+            foreach (var item in Products)
+            {
+                Product clone = new DronesProblem.Product(item.Key);
+                this.Products.Add(clone, item.Value);
+            }
+        }
+    }
+}
