1fc6c89f66898c8c57611ae5955cd921e1f79ea9
Gilthans <gilthans@gmail.com>
2017-02-23 20:57:30 +0200

    xcx

--- GoogleHashCode/2017_Qualification/Solver.cs
+++ GoogleHashCode/2017_Qualification/Solver.cs
@@ -13,14 +13,14 @@ namespace _2017_Qualification
 		private ProblemOutput _output;
 
 		private Dictionary<RequestsDescription, double> _currentTime;
-		private Dictionary<RequestsDescription, double> _bestTime;
+		private Dictionary<RequestsDescription, Tuple<CachedServer, double>> _bestTime;
 
 		protected override ProblemOutput Solve(ProblemInput input)
 		{
 			_input = input;
 			_output = new ProblemOutput { ServerAssignments = new Dictionary<CachedServer, List<Video>>() };
 			_currentTime = new Dictionary<RequestsDescription, double>();
-			_bestTime = new Dictionary<RequestsDescription, double>();
+			_bestTime = new Dictionary<RequestsDescription, Tuple<CachedServer, double>>();
 
 			while (true)
 			{
@@ -47,13 +47,15 @@ namespace _2017_Qualification
 
 		private void AssignVideoToServer(CachedServer selectedServer, RequestsDescription request)
 		{
+			Console.WriteLine("Assigned");
 			selectedServer.Capacity -= request.Video.Size;
 			_output.ServerAssignments.GetOrCreate(selectedServer, _ => new List<Video>()).Add(request.Video);
 
 			foreach(var rr in _input.RequestsDescriptions.Where(r => Equals(r.Video, request.Video)))
-			{
 				_currentTime.Remove(rr);
-			}
+
+			foreach (var rr in _bestTime.Where(kvp => Equals(kvp.Value.Item1, selectedServer)).ToList())
+				_bestTime.Remove(rr.Key);
 		}
 
 		private double CalculateServerTimeForRequest(CachedServer cachedServer, RequestsDescription request)
@@ -77,13 +79,15 @@ namespace _2017_Qualification
 		private double CalculateRequestValue(RequestsDescription requestsDescription)
 		{
 			double currentTime = _currentTime.GetOrCreate(requestsDescription, CalculateCurrentTime);
-			double bestTime = GetBestTimeForRequest(requestsDescription);
+			double bestTime = _bestTime.GetOrCreate(requestsDescription, GetBestTimeForRequest).Item2;
 			return requestsDescription.NumOfRequests * (bestTime - currentTime) / requestsDescription.Video.Size;
 		}
 
-		private double GetBestTimeForRequest(RequestsDescription requestsDescription)
+		private Tuple<CachedServer, double> GetBestTimeForRequest(RequestsDescription requestsDescription)
 		{
-			return _input.CachedServers.Min(s => CalculateServerTimeForRequest(s, requestsDescription));
+			double time;
+			var server = _input.CachedServers.ArgMin(s => CalculateServerTimeForRequest(s, requestsDescription), out time);
+			return new Tuple<CachedServer, double>(server, time); 
 		}
 
 		private double CalculateCurrentTime(RequestsDescription requestsDescription)
