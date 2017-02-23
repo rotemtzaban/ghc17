0f1c07566b44de30adbf323087dd4dbdd74cdf9e
shachar har zvi <shachar-har-zvi@hotmail.com>
2017-02-23 20:25:41 +0200

    gfd

--- GoogleHashCode/2017_Qualification_Test/ParserTests.cs
+++ GoogleHashCode/2017_Qualification_Test/ParserTests.cs
@@ -18,7 +18,23 @@ namespace _2017_Qualification_Test
             Assert.AreEqual(4, input.NumberOfRequestDescription);
             Assert.AreEqual(5, input.NumberOfVideos);
             Assert.AreEqual(100, input.ServerCapacity);
-            Assert.Fail();
+
+            Assert.AreEqual(50, input.Videos[0].Size);
+            Assert.AreEqual(50, input.Videos[1].Size);
+            Assert.AreEqual(80, input.Videos[2].Size);
+            Assert.AreEqual(30, input.Videos[3].Size);
+            Assert.AreEqual(110, input.Videos[4].Size);
+
+            Assert.AreEqual(1000, input.Endpoints[0].DataCenterLatency);
+            Assert.AreEqual(3, input.Endpoints[0].ServersLatency.Count);
+
+            Assert.AreEqual(500, input.Endpoints[1].DataCenterLatency);
+            Assert.AreEqual(0, input.Endpoints[1].ServersLatency.Count);
+
+            Assert.AreEqual(1500, input.RequestsDescriptions[0].NumOfRequests);
+            Assert.AreEqual(1000, input.RequestsDescriptions[1].NumOfRequests);
+            Assert.AreEqual(500, input.RequestsDescriptions[2].NumOfRequests);
+            Assert.AreEqual(1000, input.RequestsDescriptions[3].NumOfRequests);
         }
     }
 }
