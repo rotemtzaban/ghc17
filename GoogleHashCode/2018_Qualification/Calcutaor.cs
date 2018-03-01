4f9c7d31a72d5efdadf4a840502b08db18b5e499
shachar <shachar-har-zvi@hotmail.com>
2018-03-01 20:35:41 +0200

    sad

--- GoogleHashCode/2018_Qualification/Calcutaor.cs
+++ GoogleHashCode/2018_Qualification/Calcutaor.cs
@@ -17,14 +17,20 @@ namespace _2018_Qualification
             ProblemOutput output = new ProblemOutput();
             output.Cars = new List<Car>();
             string s = reader.ReadLine();
+            int j = 0;
             while (s != null)
             {
+                output.Cars.Add(new Car(j));
                 string[] splited = s.Split(' ');
                 for (int i = 1; i < splited.Length; i++)
                 {
-
+                    output.Cars[j].RidesTaken.Add(input.Rides[int.Parse(splited[i])]);
                 }
+
+                j++;
             }
+
+            return output;
         }
     }
 }
