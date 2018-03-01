7ef6e6bf1948431267b8f94d04160c9fc095fa7f
shachar <shachar-har-zvi@hotmail.com>
2018-03-01 23:17:39 +0200

    Add Program.cs

--- GoogleHashCode/2018_Qualification/Solver.cs
+++ GoogleHashCode/2018_Qualification/Solver.cs
@@ -17,39 +17,52 @@ namespace _2018_Qualification
             while (true)
             {
                 bool assignedRide = false;
-                foreach (var car in input.Cars)
-                {
-                    double minScore = double.MaxValue;
-                    Ride maxRide = null;
-                    Coordinate currLoc = car.CurrentTime > 0 ? car.RidesTaken.Last().End : new Coordinate(0, 0);
-                    foreach (var ride in input.Rides)
-                    {
-                        // TODO: break cond
-                        var score = ScoreCalc.GetScore(ride, currLoc, car.CurrentTime, input);
-                        if (score < minScore)
-                        {
-                            minScore = score;
-                            maxRide = ride;
-                            assignedRide = true;
-                        }
-                    }
-
-                    if (maxRide != null)
-                    {
-                        input.Rides.Remove(maxRide);
-                        car.RidesTaken.Add(maxRide);
-                        long minStartTurn = Math.Max(car.CurrentTime + currLoc.CalcGridDistance(maxRide.Start),
-                            maxRide.StartTime);
-
-                        car.CurrentTime = minStartTurn + maxRide.Distance;
-                    }
-                }
+                IEnumerable<Car> cars = input.Cars.OrderBy(_ => _.CurrentTime).Take(input.Cars.Count * 19 / 20);
+                assignedRide = Apply(input, assignedRide, cars);
 
                 if (!assignedRide)
                 {
-                    return output;
+                    assignedRide = Apply(input, assignedRide, input.Cars);
+                    if (!assignedRide)
+                    {
+                        return output;
+                    }
                 }
             }
+        }
+
+        private static bool Apply(ProblemInput input, bool assignedRide, IEnumerable<Car> cars)
+        {
+            foreach (var car in cars)
+            // foreach (var car in input.Cars)
+            {
+                double minScore = double.MaxValue;
+                Ride maxRide = null;
+                Coordinate currLoc = car.CurrentTime > 0 ? car.RidesTaken.Last().End : new Coordinate(0, 0);
+                foreach (var ride in input.Rides)
+                {
+                    // TODO: break cond
+                    var score = ScoreCalc.GetScore(ride, currLoc, car.CurrentTime, input);
+                    if (score < minScore)
+                    {
+                        minScore = score;
+                        maxRide = ride;
+                        assignedRide = true;
+                    }
+                }
+
+                if (maxRide != null)
+                {
+                    input.Rides.Remove(maxRide);
+                    car.RidesTaken.Add(maxRide);
+                    long minStartTurn = Math.Max(car.CurrentTime + currLoc.CalcGridDistance(maxRide.Start),
+                        maxRide.StartTime);
+
+                    car.CurrentTime = minStartTurn + maxRide.Distance;
+                }
+            }
+
+            return assignedRide;
         }
     }
 }
