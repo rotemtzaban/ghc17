3f21f0fb76931bd8bb25ae0416f0e579dcc5e0a7
shachar <shachar-har-zvi@hotmail.com>
2018-03-01 20:50:58 +0200

    sd

---
+++ GoogleHashCode/2018_Qualification/ScoreCalc.cs
@@ -0,0 +1,28 @@
+﻿using HashCodeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_Qualification
+{
+    public class ScoreCalc
+    {
+        public long GetScore(Ride ride, Coordinate location, int currTime, ProblemInput input)
+        {
+            long distance = GetDistance(ride.Start, location);
+            long timeToStart = currTime - ride.StartTime;
+            timeToStart = timeToStart < 0 ? 0 : timeToStart;
+
+            long turnsToWaitTillExacStart = ride.StartTime - currTime - distance;
+
+            return (distance + timeToStart) * 10 / ride.Distance;
+        }
+
+        public long GetDistance(Coordinate x, Coordinate y)
+        {
+            return Math.Abs(x.X - y.X) + Math.Abs(x.Y - y.Y);
+        }
+    }
+}
