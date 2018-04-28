feece88412e29a458b5cf353460df26d13c051d2
rotem <rotemzzz@gmail.com>
2018-04-28 11:43:18 +0100

    Add input

--- GoogleHashCode/2018_Final/ProblemInput.cs
+++ GoogleHashCode/2018_Final/ProblemInput.cs
@@ -2,5 +2,29 @@
 {
     public class ProblemInput
     {
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int MaxDistance { get; set; }
+
+        public BuildingProject[] BuildingProjects { get; set; }
+    }
+
+    public class BuildingProject
+    {
+        public bool[,] Plan { get; set; }
+
+        public BuildingType BuildingType { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int UtilityType { get; set; }
+    }
+
+    public enum BuildingType
+    {
+        Residential,
+        Utility
     }
 }
