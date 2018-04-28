791c5b61a0af016de2f257baae4b76529b7cddb0
shachar <shachar-har-zvi@hotmail.com>
2018-04-28 11:13:59 +0100

    s

--- GoogleHashCode/2018_Final/Program.cs
+++ GoogleHashCode/2018_Final/Program.cs
@@ -13,8 +13,19 @@ namespace _2018_Final
         {
             Runner<ProblemInput, ProblemOutput> runner1 = new Runner<ProblemInput, ProblemOutput>(
                 "2018_Final", new Parser(), new Solver(), new Printer(), new Calcutaor());
+            // runner1.Run(Properties.Resources.A, "A", 1, true);
 
-            // runner1.Run(Properties.Resources.charleston_road, "something", 1, true);
+            Runner<ProblemInput, ProblemOutput> runner2 = new Runner<ProblemInput, ProblemOutput>(
+                "2018_Final", new Parser(), new Solver(), new Printer(), new Calcutaor());
+            // runner2.Run(Properties.Resources.B, "B", 1, true);
+
+            Runner<ProblemInput, ProblemOutput> runner3 = new Runner<ProblemInput, ProblemOutput>(
+                "2018_Final", new Parser(), new Solver(), new Printer(), new Calcutaor());
+            // runner3.Run(Properties.Resources.C, "C", 1, true);
+
+            Runner<ProblemInput, ProblemOutput> runner4 = new Runner<ProblemInput, ProblemOutput>(
+                "2018_Final", new Parser(), new Solver(), new Printer(), new Calcutaor());
+            // runner4.Run(Properties.Resources.D, "D", 1, true);
 
             // DataAnalyze();
 
