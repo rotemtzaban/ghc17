45461782fcf64294df0477c8155227e4a83376d9
rotem <rotemzzz@gmail.com>
2018-04-28 12:09:36 +0100

    Add Output

--- GoogleHashCode/2018_Final/ProblemOutput.cs
+++ GoogleHashCode/2018_Final/ProblemOutput.cs
@@ -1,6 +1,16 @@
-﻿namespace _2018_Final
+﻿using HashCodeCommon;
+
+namespace _2018_Final
 {
     public class ProblemOutput
     {
+        public OutputBuilding[] Buildings { get; set; }
+    }
+
+    public class OutputBuilding
+    {
+        public int ProjectNumber { get; set; }
+
+        public MatrixCoordinate Coordinate { get; set; }
     }
 }
