c671fe0002e8ccc659e65e8f0b8b172bcc36511e
rotem <rotemzzz@gmail.com>
2019-02-13 20:33:42 +0200

    added stuff to problem input

--- GoogleHashCode/2016_Final/Parser.cs
+++ GoogleHashCode/2016_Final/Parser.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using HashCodeCommon.HelperClasses;
 
 namespace _2016_Final
 {
@@ -12,7 +13,15 @@ namespace _2016_Final
     {
         protected override ProblemInput ParseFromStream(TextReader reader)
         {
-            throw new NotImplementedException();
+            var satCount = reader.GetInt();
+            Satallite[] satallites = new Satallite[satCount];
+            for (int i = 0; i < satCount; i++)
+            {
+                var longList = reader.GetLongList();
+                satallites[i] = new Satallite(i, longList[0], longList[1], longList[2], longList[3], longList[4]);
+            }
+
+
         }
     }
 }
