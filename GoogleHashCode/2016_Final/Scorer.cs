afbcfd620e57ee7e184043f80951ab37dfb7e158
Gal <galle@DESKTOP-66VM20E>
2019-02-13 20:53:25 +0200

    Scorer finished stupid impl

--- GoogleHashCode/2016_Final/Scorer.cs
+++ GoogleHashCode/2016_Final/Scorer.cs
@@ -26,14 +26,21 @@ namespace _2016_Final
 
         private bool IsCollectionCovered(Collection collection, ProblemInput input, ProblemOutput output)
         {
-            throw new NotImplementedException();
+            foreach (var location in collection.Locations)
+            {
+                if (!output.ImagesTaken.Any(_ => _.Latitude == location.Lat && _.Longitude == location.Lon))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public override ProblemOutput GetResultFromReader(ProblemInput input, TextReader reader)
         {
             var numOfPicsTaken = long.Parse(reader.ReadLine());
 
-            var list = new HashSet<ImageTakenDetails>();
+            var list = new List<ImageTakenDetails>();
 
             while (true)
             {
