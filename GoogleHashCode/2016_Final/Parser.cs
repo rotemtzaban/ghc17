56d7de30b358763aebe355187e2c4c01169fa25e
rotem <rotemzzz@gmail.com>
2019-02-13 21:01:19 +0200

    added parser

--- GoogleHashCode/2016_Final/Parser.cs
+++ GoogleHashCode/2016_Final/Parser.cs
@@ -21,7 +21,36 @@ namespace _2016_Final
                 satallites[i] = new Satallite(i, longList[0], longList[1], longList[2], longList[3], longList[4]);
             }
 
+            var colCount = reader.GetInt();
+            Collection[] collections = new Collection[colCount];
+            for (int i = 0; i < colCount; i++)
+            {
+                var longList = reader.GetLongList();
+                long value = longList[0], locationCount = longList[1], timeRangeCount = longList[2];
+                var locations = new Location[locationCount];
+                for (int j = 0; j < locationCount; j++)
+                {
+                    var location = reader.GetLongList();
+                    locations[j] = new Location { Lat = location[0], Lon = location[1] };
+                }
 
+                TimeRange[] timeRanges = new TimeRange[timeRangeCount];
+
+                for (int j = 0; j < timeRangeCount; j++)
+                {
+                    var timeRange = reader.GetLongList();
+                    timeRanges[j] = new TimeRange { Start = timeRange[0], End = timeRange[1] };
+                }
+
+                collections[i] = new Collection {TimeRanges = timeRanges, Locations = locations, Value = value};
+            }
+
+
+            return new ProblemInput
+            {
+                Satallites = satallites,
+                Collections = collections
+            };
         }
     }
 }
