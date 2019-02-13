1dfb5f889806dcf1c307553e9343d96bbeb7620c
Meni Orenbach <shmenio@gmail.com>
2019-02-13 20:32:06 +0200

    calc position

--- GoogleHashCode/2016_Final/ProblemInput.cs
+++ GoogleHashCode/2016_Final/ProblemInput.cs
@@ -64,8 +64,49 @@ namespace _2016_Final
 
         public void CalcPosition(long turn)
         {
-            throw new NotImplementedException();
+            // TODO: caching!!
+            long curr_lat = this.InitialLat;
+            long curr_lon = this.InitialLon;
+            long curr_velocity = this.InitialVelocity;
 
+            for (int i=0;i<turn;i++)
+            {
+                long comp = curr_lat + curr_velocity;
+                long lat_next;
+                long lon_next;
+                long velocity_next;
+                if (comp >= -324000 && comp < 324000)
+                {
+                    lat_next = comp;                    
+                    lon_next = curr_lon - 90 * 3600;
+                    velocity_next = curr_velocity;                    
+                }
+                else if (comp > 324000)
+                {
+                    lat_next = 180 * 3600 - comp;
+                    lon_next = -180 * 3600 + (curr_lon - 90 * 3600);
+                    velocity_next = -curr_velocity;
+                }
+                else // if (comp < -324000)
+                {
+                    lat_next = -180 * 3600 - comp;
+                    lon_next = -180 * 3600 + (curr_lon - 90 * 3600);
+                    velocity_next = -curr_velocity;
+                }
+
+                // prep for next iteration
+                curr_lat = lat_next;
+                curr_lon = lon_next;
+                curr_velocity = velocity_next;
+
+                // final iteration - update properties
+                if (i == turn -1)
+                {
+                    this.Lat = curr_lat;
+                    this.Lon = curr_lon;
+                    this.Velocity = curr_velocity;
+                }
+            }            
         }
     }
 }
