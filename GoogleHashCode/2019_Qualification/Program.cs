c97643f317d44f0bd4dd09d25a75b780cd4af71d
Meni Orenbach <shmenio@gmail.com>
2019-02-21 16:22:16 +0200

    create template project for 2019 qualification round

---
+++ GoogleHashCode/2019_Qualification/Program.cs
@@ -0,0 +1,23 @@
+﻿using HashCodeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019_Qualification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner<ProblemInput, ProblemOutput> runner1 = new Runner<ProblemInput, ProblemOutput>(
+                "2019", new Parser(), new Solver(), new Printer(), new Calcutaor());
+            //runner1.Run(Properties.Resources.input_1, "input_1", 1, true);
+
+            ZipCreator.CreateCodeZip(string.Empty);
+
+            Console.Read();
+        }
+    }
+}
