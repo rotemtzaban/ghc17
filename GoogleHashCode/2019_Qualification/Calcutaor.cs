532ffb884fc59dceae39de066edd964a68d430cc
Meni Orenbach <shmenio@gmail.com>
2019-02-28 20:36:43 +0200

    BUGS

--- GoogleHashCode/2019_Qualification/Calcutaor.cs
+++ GoogleHashCode/2019_Qualification/Calcutaor.cs
@@ -66,7 +66,7 @@ namespace _2019_Qualification
                 slides[i] = new Slide(slide);
             }
 
-            return new ProblemOutput { Slides = new List<Slide>() };
+            return new ProblemOutput { Slides = slides.ToList() };
         }
     }
 }
