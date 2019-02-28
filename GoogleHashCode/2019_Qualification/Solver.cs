a25efe98e34f91a72fbffb4adcaa694483f3853b
ipdan96 <ipdan96@gmail.com>
2019-02-28 20:52:22 +0200

    add idan solver

--- GoogleHashCode/2019_Qualification/Solver.cs
+++ GoogleHashCode/2019_Qualification/Solver.cs
@@ -12,45 +12,62 @@ namespace _2019_Qualification
     {
         protected override ProblemOutput Solve(ProblemInput input)
         {
-            ProblemOutput res = new ProblemOutput();
-            res.Slides = new List<Slide>();
-            res.Slides.Add(new Slide(new List<Photo>() { new Photo(0, false, null) }));
-            res.Slides.Add(new Slide(new List<Photo>() { new Photo(3, false, null) }));
-            res.Slides.Add(new Slide(new List<Photo>() { new Photo(1, true, null), new Photo(2, true, null) }));
+            //ProblemOutput res = new ProblemOutput();
+            //res.Slides = new List<Slide>();
+            //res.Slides.Add(new Slide(new List<Photo>() { new Photo(0, false, null) }));
+            //res.Slides.Add(new Slide(new List<Photo>() { new Photo(3, false, null) }));
+            //res.Slides.Add(new Slide(new List<Photo>() { new Photo(1, true, null), new Photo(2, true, null) }));
 
-            return res;
+            //return res;
+
+            return Solve2(input);
         }
 
         protected ProblemOutput Solve2(ProblemInput input)
         {
+            ProblemOutput res = new ProblemOutput();
+            res.Slides = new List<Slide>();
             HashSet<int> takenPhotos = new HashSet<int>();
-            List<Slide> slideShow = new List<Slide>();
 
             int count = 0;
             int index = 0;
             while (count < input.Photos.Length && index < input.Photos.Length)
             {
-                var photo = input.Photos[index];
+                Photo photo;
+                if (res.Slides.Count != 0)
+                {
+                    photo = res.Slides[res.Slides.Count -1].Images[0];
+                }
+                else
+                {
+                    photo = input.Photos[index];
+                }
                 takenPhotos.Add(photo.Index);
-                Photo getNextSlide = GetNextSlide(takenPhotos, input, photo);
-                if (getNextSlide != null)
+                Photo nextSlide = GetNextSlide(takenPhotos, input, photo);
+                if (nextSlide == null)
                 {
                     takenPhotos.Remove(photo.Index);
                     index++;
                 }
                 else
                 {
+                    res.Slides.Add(new Slide(new List<Photo>() { photo }));
+                    res.Slides.Add(new Slide(new List<Photo>(){ nextSlide}));
                     count++;
                 }
             }
 
-            return null;
+            return res;
         }
 
         private Photo GetNextSlide(HashSet<int> takenPhotos, ProblemInput input, Photo firstPhoto)
         {
             foreach (var photo in input.Photos)
             {
+                if (photo.IsVertical)
+                {
+                    continue;
+                }
                 if (!takenPhotos.Contains(photo.Index))
                 {
                     if (MoreThanX(firstPhoto, photo))
