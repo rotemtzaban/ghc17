a9a6ed09ff1f101fba33de175e63009bb0b6d85c
Gal <galle@DESKTOP-66VM20E>
2019-02-28 20:13:48 +0200

    Tags of slide

--- GoogleHashCode/2019_Qualification/Slide.cs
+++ GoogleHashCode/2019_Qualification/Slide.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace _2019_Qualification
 {
     public class Slide
     {
         public List<Photo> Images { get; set; }
+
+        public IEnumerable<string> Tags
+        {
+            get
+            {
+                return this.Images.SelectMany(_ => _.Tags).Distinct();
+            }
+        }
     }
 }
