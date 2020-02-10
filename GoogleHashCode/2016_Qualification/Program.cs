aa6888a67a20417a59cc64a4bae2ee0fbac09623
shachar <shachar-har-zvi@hotmail.com>
2020-02-10 20:13:49 +0200

    gd

--- GoogleHashCode/2016_Qualification/Program.cs
+++ GoogleHashCode/2016_Qualification/Program.cs
@@ -12,14 +12,16 @@ namespace _2016_Qualification
     {
         static void Main(string[] args)
         {
-            var runner1 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), new Calculator());
+            Calculator calculator = null;
+
+            var runner1 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
             runner1.Run(Resources.busy_day, "");
 
-            var runner2 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), new Calculator());
-            runner2.Run(Resources.busy_day, "");
+            var runner2 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            runner2.Run(Resources.mother_of_all_warehouses, "");
 
-            var runner3 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), new Calculator());
-            runner3.Run(Resources.busy_day, "");
+            var runner3 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            runner3.Run(Resources.redundancy, "");
 
         }
     }
