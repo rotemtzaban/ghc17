a45dba222cb4884882fce2866e757a7028f1f4ec
idanperez <ipdan96@gmail.com>
2020-02-10 20:33:16 +0200

    calculator maybw works

--- GoogleHashCode/2016_Qualification/Program.cs
+++ GoogleHashCode/2016_Qualification/Program.cs
@@ -12,7 +12,7 @@ namespace _2016_Qualification
     {
         static void Main(string[] args)
         {
-            Calculator calculator = null;
+            Calculator calculator = new Calculator();
 
             var runner1 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
             runner1.Run(Resources.busy_day, "");
