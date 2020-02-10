ce7731a345c542169560137971c476daf74ba0f8
shachar <shachar-har-zvi@hotmail.com>
2020-02-10 18:47:04 +0200

    fdg

---
+++ GoogleHashCode/2016_Qualification/ScoreCalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_Qualification
+{
+    public static class ScoreCalc
+    {
+        public static double Calc()
+        {
+            return 0;
+        }
+    }
+}
