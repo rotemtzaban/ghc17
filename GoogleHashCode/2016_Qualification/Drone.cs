96fe7e9613d04e7a06a45a4ab3b7240632935c0b
idanperez <ipdan96@gmail.com>
2020-02-10 19:02:39 +0200

    added commands

--- GoogleHashCode/2016_Qualification/Drone.cs
+++ GoogleHashCode/2016_Qualification/Drone.cs
@@ -1,14 +1,100 @@
-﻿using HashCodeCommon;
+﻿using System.Collections.Generic;
+using HashCodeCommon;
 
 namespace _2016_Qualification
 {
     class Drone : IndexedObject
     {
+        public List<ICommand> Commands { get; set; }
+
         public MatrixCoordinate CurrentPosition { get; set; }
 
         public Drone(int index) : base(index)
         {
             CurrentPosition = new MatrixCoordinate(0, 0);
+            Commands = new List<ICommand>();
         }
+    }
+}
+
+public interface ICommand { }
+
+public class Load : ICommand
+{
+    public int DroneId { get; }
+    public int WarehouseId { get; }
+    public int ProductId { get; }
+    public int CountOfProducts { get; }
+
+    public Load(int droneId, int warehouseId, int productId, int countOfProducts)
+    {
+        DroneId = droneId;
+        WarehouseId = warehouseId;
+        ProductId = productId;
+        CountOfProducts = countOfProducts;
+    }
+
+    public override string ToString()
+    {
+        return $"{DroneId} L {WarehouseId} {ProductId} {CountOfProducts}";
+    }
+}
+
+public class Unload : ICommand
+{
+    public int DroneId { get; }
+    public int WarehouseId { get; }
+    public int ProductId { get; }
+    public int CountOfProducts { get; }
+
+    public Unload(int droneId, int warehouseId, int productId, int countOfProducts)
+    {
+        DroneId = droneId;
+        WarehouseId = warehouseId;
+        ProductId = productId;
+        CountOfProducts = countOfProducts;
+    }
+
+    public override string ToString()
+    {
+        return $"{DroneId} U {WarehouseId} {ProductId} {CountOfProducts}";
+    }
+}
+
+public class Deliver : ICommand
+{
+    public int DroneId { get; }
+    public int OrderId { get; }
+    public int ProductId { get; }
+    public int CountOfProducts { get; }
+
+    public Deliver(int droneId, int orderId, int productId, int countOfProducts)
+    {
+        DroneId = droneId;
+        OrderId = orderId;
+        ProductId = productId;
+        CountOfProducts = countOfProducts;
+    }
+
+    public override string ToString()
+    {
+        return $"{DroneId} D {OrderId} {ProductId} {CountOfProducts}";
+    }
+}
+
+public class Wait : ICommand
+{
+    public int DroneId { get; }
+    public int NumberOfTurnsToWait { get; }
+
+    public Wait(int droneId, int numberOfTurnsToWait)
+    {
+        DroneId = droneId;
+        NumberOfTurnsToWait = numberOfTurnsToWait;
+    }
+
+    public override string ToString()
+    {
+        return $"{DroneId} W {NumberOfTurnsToWait}";
     }
 }
