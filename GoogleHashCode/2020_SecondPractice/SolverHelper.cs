c6a4cc897f6ce0772ed64c008ab883664773d334
idanperez <ipdan96@gmail.com>
2020-02-17 19:16:07 +0200

    added Pools Object

--- GoogleHashCode/2020_SecondPractice/SolverHelper.cs
+++ GoogleHashCode/2020_SecondPractice/SolverHelper.cs
@@ -33,42 +33,43 @@ namespace _2020_SecondPractice
                 poolToCapacity[server.PoolAssigned.Value] += server.Capacity;
             }
 
-            List<PoolDetails> poolDetails = new List<PoolDetails>(input.NumOfPools);
-            for (int i = 0; i < input.NumOfPools; i++)
-            {
-                poolDetails[i] = new PoolDetails(i);
-                if (!poolToCapacity.ContainsKey(i))
-                {
-                    poolDetails[i].WorstRow = -1;
-                    poolDetails[i].GuaranteedCapacity = 0;
-                    continue;
-                }
+            //List<PoolDetails> poolDetails = new List<PoolDetails>(input.NumOfPools);
+            //for (int i = 0; i < input.NumOfPools; i++)
+            //{
+            //    poolDetails[i] = new PoolDetails(i);
+            //    if (!poolToCapacity.ContainsKey(i))
+            //    {
+            //        poolDetails[i].WorstRow = -1;
+            //        poolDetails[i].GuaranteedCapacity = 0;
+            //        continue;
+            //    }
 
-                long minGcPool = poolToCapacity[i];
-                long bestRowForPool = long.MinValue;
-                for (int j = 0; j < input.NumOfRows; j++)
-                {
-                    long rowForPool = rowToServers.ContainsKey(j)
-                        ? rowToServers[j].Sum(_ => input.Servers[_].Capacity)
-                        : 0;
-                    long currentRowDownGc = poolToCapacity[i] - rowForPool;
+            //    long minGcPool = poolToCapacity[i];
+            //    long bestRowForPool = long.MinValue;
+            //    for (int j = 0; j < input.NumOfRows; j++)
+            //    {
+            //        long rowForPool = rowToServers.ContainsKey(j)
+            //            ? rowToServers[j].Sum(_ => input.Servers[_].Capacity)
+            //            : 0;
+            //        long currentRowDownGc = poolToCapacity[i] - rowForPool;
 
-                    if (bestRowForPool < rowForPool)
-                    {
-                        bestRowForPool = rowForPool;
-                        poolDetails[i].BestRow = j;
-                    }
+            //        if (bestRowForPool < rowForPool)
+            //        {
+            //            bestRowForPool = rowForPool;
+            //            poolDetails[i].BestRow = j;
+            //        }
 
-                    if (currentRowDownGc >= minGcPool) continue;
-                    minGcPool = currentRowDownGc;
-                    poolDetails[i].WorstRow = j;
-                }
+            //        if (currentRowDownGc >= minGcPool) continue;
+            //        minGcPool = currentRowDownGc;
+            //        poolDetails[i].WorstRow = j;
+            //    }
 
-                poolDetails[i].GuaranteedCapacity = minGcPool;
-            }
+            //    poolDetails[i].GuaranteedCapacity = minGcPool;
+            //}
 
-            poolDetails.Sort();
-            return poolDetails;
+            //poolDetails.Sort();
+            //return poolDetails;
+            return null;
         }
 
         public static void ImproveWorstPoolWorstRow(ProblemInput input, ProblemOutput output2)
@@ -96,21 +97,94 @@ namespace _2020_SecondPractice
 
     public class PoolDetails : IndexedObject
     {
-        public PoolDetails(int index) : base(index)
+        public PoolDetails(int index, int numberOfRows) : base(index)
         {
+            RowsCapacity = new List<int>(numberOfRows);
         }
 
-        public int WorstRow { get; set; }
-        public int BestRow { get; set; }
-        public long GuaranteedCapacity { get; set; }
+        private bool isAllUpdated = false;
+        private long _GuaranteedCapacity = long.MinValue;
+        private int _WorstRow = -1;
+        private int _BestRow = -1;
+
+        public int WorstRow
+        {
+            get
+            {
+                if (isAllUpdated)
+                {
+                    return _WorstRow;
+                }
+                else
+                {
+                    UpdateAll();
+                    return _WorstRow;
+                }
+            }
+        }
+        public int BestRow
+        {
+            get
+            {
+                if (isAllUpdated)
+                {
+                    return _BestRow;
+                }
+                else
+                {
+                    UpdateAll();
+                    return _BestRow;
+                }
+            }
+        }
+
+        public long GuaranteedCapacity
+        {
+            get
+            {
+                if (isAllUpdated)
+                {
+                    return _GuaranteedCapacity;
+                }
+                else
+                {
+                    UpdateAll();
+                    return _GuaranteedCapacity;
+                }
+            }
+        }
+
+        private void UpdateAll()
+        {
+            _GuaranteedCapacity = Capacity;
+            long bestRowForPool = long.MinValue;
+            for (int i = 0; i < RowsCapacity.Count(); i++)
+            {
+                long currentRowDownGc = Capacity - RowsCapacity[i];
+
+                if (bestRowForPool < RowsCapacity[i])
+                {
+                    bestRowForPool = RowsCapacity[i];
+                    _BestRow = i;
+                }
+
+                if (currentRowDownGc >= _GuaranteedCapacity) continue;
+                _GuaranteedCapacity = currentRowDownGc;
+                _WorstRow = i;
+            }
+
+            isAllUpdated = true;
+        }
+
         public long Capacity { get; set; }
-        public List<int> RowsCapacity { get; set; } = new List<int>();
+        public List<int> RowsCapacity { get; set; }
 
         public void AddServerToPool(Server server)
         {
             server.PoolAssigned = this.Index;
             RowsCapacity[server.Row.Value] += server.Capacity;
             Capacity += server.Capacity;
+            isAllUpdated = false;
         }
 
         public void RemoveServerFromPool(Server server)
@@ -118,6 +192,7 @@ namespace _2020_SecondPractice
             server.PoolAssigned = null;
             RowsCapacity[server.Row.Value] -= server.Capacity;
             Capacity -= server.Capacity;
+            isAllUpdated = false;
         }
     }
 }
