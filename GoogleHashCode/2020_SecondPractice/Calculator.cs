72edac0a67a83627bca87cb4f044286a60e9ae9c
idanperez <ipdan96@gmail.com>
2020-02-17 18:17:06 +0200

    Calculator Finished

--- GoogleHashCode/2020_SecondPractice/Calculator.cs
+++ GoogleHashCode/2020_SecondPractice/Calculator.cs
@@ -12,29 +12,38 @@ namespace _2020_SecondPractice
     {
         public override long Calculate(ProblemInput input, ProblemOutput output)
         {
-            Dictionary<int, HashSet<int>> rowToServers = new Dictionary<int, HashSet<int>>();
+            Dictionary<int, List<int>> rowToServers = new Dictionary<int, List<int>>();
             Dictionary<int, HashSet<int>> poolToServer = new Dictionary<int, HashSet<int>>();
+            Dictionary<int, long> poolToCapacity = new Dictionary<int, long>();
             foreach (var server in output.Servers)
             {
                 if (server.Row == null) continue;
                 if (!rowToServers.ContainsKey(server.Row.Value))
                 {
-                    rowToServers[server.Row.Value] = new HashSet<int>();
+                    rowToServers[server.Row.Value] = new List<int>();
                 }
 
                 if (!poolToServer.ContainsKey(server.PoolAssigned.Value))
                 {
                     poolToServer[server.PoolAssigned.Value] = new HashSet<int>();
+                    poolToCapacity[server.PoolAssigned.Value] = 0;
                 }
 
                 poolToServer[server.PoolAssigned.Value].Add(server.Index);
                 rowToServers[server.Row.Value].Add(server.Index);
+                poolToCapacity[server.PoolAssigned.Value] += server.Capacity;
             }
 
             long minGc = long.MaxValue;
-
             for (int i = 0; i < input.NumOfPools; i++)
             {
+                long minGcPool = poolToCapacity[i];
+                for (int j = 0; j < input.NumOfRows; j++)
+                {
+                    long currentRowDownGc = poolToCapacity[i] - rowToServers[j].Sum(_ => input.Servers[_].Capacity);
+                    minGcPool = Math.Min(minGcPool, currentRowDownGc);
+                }
+                minGc = Math.Min(minGcPool, minGc);
             }
 
             return minGc;
