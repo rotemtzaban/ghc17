58a2d3b97ef10648b497f3b16a61337c58443320
Rotem Tzaban <rotemtzaban@gmail.com>
2020-02-17 19:36:36 +0200

    Shit

--- GoogleHashCode/2020_SecondPractice/Printer.cs
+++ GoogleHashCode/2020_SecondPractice/Printer.cs
@@ -12,17 +12,17 @@ namespace _2020_SecondPractice
     {
         public override void PrintToConsole(ProblemOutput result)
         {
-            foreach (var server in result.Servers)
-            {
-                if (server.Row == null)
-                {
-                    Console.WriteLine("x");
-                }
-                else
-                {
-                    Console.WriteLine($"{server.Index} {server.Row} {server.SlotInRow}");
-                }
-            }
+            //foreach (var server in result.Servers)
+            //{
+            //    if (server.Row == null)
+            //    {
+            //        Console.WriteLine("x");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{server.Index} {server.Row} {server.SlotInRow}");
+            //    }
+            //}
         }
 
         public override void PrintToFile(ProblemOutput result, string outputPath)
