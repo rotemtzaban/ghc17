b29893ae702233abcff95a29621f56d4293d2ec4
shachar <shachar-har-zvi@hotmail.com>
2020-02-17 17:35:11 +0200

    ds

---
+++ GoogleHashCode/2020_SecondPractice/Parser.cs
@@ -0,0 +1,18 @@
+﻿using HashCodeCommon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_SecondPractice
+{
+    public class Parser : ParserBase<ProblemInput>
+    {
+        protected override ProblemInput ParseFromStream(TextReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GoogleHashCode/2020_SecondPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using HashCodeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_SecondPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = null;
+
+            var runner1 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            // runner1.Run(Resources.busy_day, "busy_day");
+
+            var runner2 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            // runner2.Run(Resources.mother_of_all_warehouses, "mother_of_all_warehouses");
+
+            var runner3 = new Runner<ProblemInput, ProblemOutput>("2016_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            // runner3.Run(Resources.redundancy, "redundancy");
+
+            Console.ReadLine();
+        }
+    }
+}
