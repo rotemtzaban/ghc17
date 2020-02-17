0d2026be211156be529fdf91d2990f2c07f63e14
shachar <shachar-har-zvi@hotmail.com>
2020-02-17 17:48:09 +0200

    fsa

--- GoogleHashCode/2020_SecondPractice/ProblemInput.cs
+++ GoogleHashCode/2020_SecondPractice/ProblemInput.cs
@@ -8,5 +8,11 @@ namespace _2020_SecondPractice
 {
     public class ProblemInput
     {
+        public bool[,] Slots { get; set; }
+        public int NumOfRows { get; set; }
+        public int RowSize { get; set; }
+        public int NumOfServers { get; set; }
+        public int NumberOfPools { get; set; }
+        public List<Server> Servers { get; set; }
     }
 }
---
+++ GoogleHashCode/2020_SecondPractice/Server.cs
@@ -0,0 +1,11 @@
+﻿using HashCodeCommon;
+
+namespace _2020_SecondPractice
+{
+    public class Server : IndexedObject
+    {
+        public Server(int index) : base(index)
+        {
+        }
+    }
+}
