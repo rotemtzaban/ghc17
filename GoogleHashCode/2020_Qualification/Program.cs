cc84c57a7f1e29a8e01e031d92ea24e9c93007ad
idanperez <ipdan96@gmail.com>
2020-02-20 18:28:21 +0200

    one solver for faster change

--- GoogleHashCode/2020_Qualification/Program.cs
+++ GoogleHashCode/2020_Qualification/Program.cs
@@ -13,15 +13,15 @@ namespace _2020_Qualification
         static void Main(string[] args)
         {
             Calculator calculator = null;
-
-            var runner1 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            Solver solver = new Solver();
+            var runner1 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), solver, new Printer(), calculator);
             runner1.Run(Resources.Example, "Example");
             // runner1.Run(Resources.busy_day, "busy_day");
 
-            var runner2 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            var runner2 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), solver, new Printer(), calculator);
             // runner2.Run(Resources.mother_of_all_warehouses, "mother_of_all_warehouses");
 
-            var runner3 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), new Solver(), new Printer(), calculator);
+            var runner3 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), solver, new Printer(), calculator);
             // runner3.Run(Resources.redundancy, "redundancy");
 
             Console.ReadLine();
