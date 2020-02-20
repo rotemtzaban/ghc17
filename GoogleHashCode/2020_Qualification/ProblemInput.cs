ee77d01319193fd301e2bdf94102aec2c212d37a
shachar <shachar-har-zvi@hotmail.com>
2020-02-20 19:51:32 +0200

    fsd

--- GoogleHashCode/2020_Qualification/ProblemInput.cs
+++ GoogleHashCode/2020_Qualification/ProblemInput.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HashCodeCommon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace _2020_Qualification
 {
     public class ProblemInput
     {
+        public int NumberOfBooks { get; set; }
+        public int NumberOfLibraries { get; set; }
+        public int NumberOfDays { get; set; }
+        public List<Book> Books { get; set; }
+
+    }
+
+    public class Book : IndexedObject
+    {
+        public Book(int index) : base(index)
+        {
+        }
+    }
+
+    public class Library
+    {
+        public int NumberOfBooks { get; set; }
+        public List<Book> books { get; set; }
+        public int BooksPerDay { get; set; }
+        public int LibrarySignupTime { get; set; }
     }
 }
