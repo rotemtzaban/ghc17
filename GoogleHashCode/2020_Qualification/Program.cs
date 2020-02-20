181b09f5616d5714685656b37341b76e8aa9c739
shachar <shachar-har-zvi@hotmail.com>
2020-02-20 20:01:30 +0200

    fds

--- GoogleHashCode/2020_Qualification/Program.cs
+++ GoogleHashCode/2020_Qualification/Program.cs
@@ -14,9 +14,9 @@ namespace _2020_Qualification
         {
             Calculator calculator = null;
             Solver solver = new Solver();
+
             var runner1 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), solver, new Printer(), calculator);
             runner1.Run(Resources.Example, "Example");
-            // runner1.Run(Resources.busy_day, "busy_day");
 
             var runner2 = new Runner<ProblemInput, ProblemOutput>("2020_Qualification", new Parser(), solver, new Printer(), calculator);
             // runner2.Run(Resources.mother_of_all_warehouses, "mother_of_all_warehouses");
